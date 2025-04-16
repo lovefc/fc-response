708f3cce8d992b0e7fba3c1c3e996e0762e12890
lovefc <fcphp@qq.com>
2025-04-16 22:14:27 +0800

    0.0.1版本

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author       : lovefc
+ * @Blog         : http://lovefc.cn
+ * @Email        : fcphp@qq.com
+ * @Date         : 2025-04-16 11:14:56
+ * @LastEditTime : 2025-04-16 11:14:56
+ */
+
+const response = require('./lib/response.js');
+
+module.exports = response;
